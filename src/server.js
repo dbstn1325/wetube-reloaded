fe48d23f7715e577431bb1de5a6fcc4ce4026369
Jung Yoon Soo <dbstn6477@gmail.com>
2021-07-24 16:19:14 +0900

    Router Commit

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "../routers/globalRouter";
+import userRouter from "../routers/userRotuer";
+import videoRouter from "../routers/videoRouter";
 
 const PORT = 4000;
 
@@ -8,18 +11,11 @@ const logger = morgan("dev");
 
 const handleListening = () => console.log(`Listening to ${PORT}`)
 
-const home = (req, res) =>{
-    console.log("I will respond.");
-    return res.send("Hey");
-}
-const login = (req, res)=>{
-    return res.send("login");
-}
-
 
 
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 app.listen(PORT, handleListening); 
