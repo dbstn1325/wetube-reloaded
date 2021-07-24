057f4004a5adb0db38539999dc6af650643a0d70
Jung Yoon Soo <dbstn6477@gmail.com>
2021-07-24 15:54:19 +0900

    Middleware Commit

--- src/server.js
+++ src/server.js
@@ -1,8 +1,25 @@
 import express from "express";
-
-const app = express();
+import morgan from "morgan";
 
 const PORT = 4000;
+
+const app = express();
+const logger = morgan("dev");
+
 const handleListening = () => console.log(`Listening to ${PORT}`)
+
+const home = (req, res) =>{
+    console.log("I will respond.");
+    return res.send("Hey");
+}
+const login = (req, res)=>{
+    return res.send("login");
+}
+
+
+
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 app.listen(PORT, handleListening); 
