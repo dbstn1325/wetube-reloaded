159e7a91b26f3509f0e7966f4a533ce9228d4a88
Jung Yoon Soo <dbstn6477@gmail.com>
2021-07-25 14:30:07 +0900

    Router correction 2

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,7 @@
 export const home = (req, res) => res.send("Home");
 
-export const watch = (req, res) => res.send("Watch Video");
+export const upload = (req, res) => res.send("Upload Video");
+export const see = (req, res) => res.send("See Video");
 export const edit = (req, res) => res.send("Edit Video");
+export const search = (req, res) => res.send("Search Video");
+export const deleteVideo = (req, res) => res.send("Delete Video");
