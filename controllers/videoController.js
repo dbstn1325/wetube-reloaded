4e512ec6eba5e893fe25897a47ff0b4835acdf1f
Jung Yoon Soo <dbstn6477@gmail.com>
2021-07-29 18:21:05 +0900

    conditional

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,40 +1,38 @@
-const fakeUser={
-    username:"Yoon soo",
-    loggedIn:"False"
-};
+const videos = [{
+    title : "Herry Poter",
+    rating : 5,
+    comments : 2,
+    createdAt: "2 minutes ago",
+    views:59,
+    id:1,
+ },
+ {
+     title : "Azecvan",
+     rating : 5,
+     comments : 2,
+     createdAt: "2 minutes ago",
+     views:59,
+     id:2,  
+ },
+ {
+     title : "Yes am",
+     rating : 5,
+     comments : 2,
+     createdAt: "2 minutes ago",
+     views:59,
+     id:3,
+ }];
 
 
 export const home = (req, res) => {
-    const videos = [{
-       title : "First Video",
-       rating : 5,
-       comments : 2,
-       createdAt: "2 minutes ago",
-       views:59,
-       id:1,
-    },
-    {
-        title : "Second Video",
-        rating : 5,
-        comments : 2,
-        createdAt: "2 minutes ago",
-        views:59,
-        id:1,  
-    },
-    {
-        title : "Third Video",
-        rating : 5,
-        comments : 2,
-        createdAt: "2 minutes ago",
-        views:59,
-        id:1,
-    }];
-    return res.render("home", { pageTitle: "Home", fakeUser:fakeUser, videos});
+    return res.render("home", { pageTitle: "Home", videos});
 }
 
 
-export const see = (req, res) => {
-    res.render("watch", { pageTitle: "Watch"});
+export const watch = (req, res) => {
+    const { id } = req.params;
+    const video=videos[id-1];
+    res.render("watch", { pageTitle: `you Watching ${video.title}`, video});
     //res.send(`Watch Video #${req.params.id}`);
 }
 export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
