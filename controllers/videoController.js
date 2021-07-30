c0627c703f4a1e7dfa900bf8d876c94e89d25ec2
Jung Yoon Soo <dbstn6477@gmail.com>
2021-07-30 13:51:40 +0900

    db practice

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -35,11 +35,13 @@ export const watch = (req, res) => {
     res.render("watch", { pageTitle: `you Watching ${video.title}`, video});
     //res.send(`Watch Video #${req.params.id}`);
 }
+
 export const getEdit = (req, res) => {
     const { id } = req.params;
     const video=videos[id-1];
     res.render("edit", {pageTitle: "Edit", video});
 }
+
 export const postEdit = (req, res) => {
     const { id } = req.params;
     const { title } = req.body;
@@ -53,7 +55,16 @@ export const search = (req, res) => res.send("Search Video");
 
 export const getUpload = (req, res) => res.render("upload");
 export const postUpload = (req, res) => {
-    console.log(req.body);
+    const { uploadTitle } = req.body;
+    const newVideo = {
+        title:uploadTitle,
+        rating : 0,
+        comments : 0,
+        createdAt: "2 minutes ago",
+        views: 0,
+        id: videos.length + 1
+    };
+    videos.push(newVideo);
     return res.redirect("/");
 };
 
