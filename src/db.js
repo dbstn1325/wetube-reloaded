9796b395eb975a0a309521bac3bd50710d72ac7d
Jung Yoon Soo <dbstn6477@gmail.com>
2021-08-01 18:40:12 +0900

    DataBase init

---
+++ src/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connect to MongoDB!");
+db.on("error", (error) => console.log("Error!"));
+db.once("open", handleOpen);
+
