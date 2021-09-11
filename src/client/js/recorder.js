7cc3cd3438abf99c6086008ab4d8c004247f7669
dbstn1325@naver.com <dbstn1325@naver.com>
2021-09-11 15:25:22 +0900

    Recording System

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,54 @@
 const startBtn = document.getElementById("startBtn");
+const preview = document.getElementById("preview");
 
-const handleStartBtn = async() =>{
-    const stream = await navigator.mediaDevices.getUserMedia({
-        video:true,
-        audio:true
-    });
-    console.log(stream);
+let stream;
+let recorder;
+let videoSrc;
+
+const handleDownloadBtn = () => {
+    const a = document.createElement("a");
+    a.href=videoSrc;
+    a.download="MyRecording.webm";
+    document.body.appendChild(a);
+    a.click();
 }
+
+const handleStopBtn = () => {
+    startBtn.innerText = "Download Record";
+    
+    recorder.stop();    
+    startBtn.removeEventListener("click", handleStopBtn);
+    startBtn.addEventListener("click", handleDownloadBtn);
+    
+}
+
+const handleStartBtn = () =>{
+    startBtn.innerText = "Stop Recording";
+
+    recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (e) => {
+        videoSrc = URL.createObjectURL(e.data);
+        preview.src = videoSrc;
+        preview.srcObject = null;
+        preview.loop = true;
+        preview.play();
+    };
+
+    recorder.start();
+    startBtn.removeEventListener("click", handleStartBtn);
+    startBtn.addEventListener("click", handleStopBtn);
+    
+}
+
+const init = async() =>{
+    stream = await navigator.mediaDevices.getUserMedia({
+        video: { width: 100, height: 200},
+        audio: true,
+    });
+    preview.srcObject = stream;
+    preview.play();
+}
+
+init();
 
 startBtn.addEventListener("click", handleStartBtn);
